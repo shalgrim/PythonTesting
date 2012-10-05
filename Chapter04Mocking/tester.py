d791775985362169aea362e129892fda4f3d1968
shalgrim <shalgrim@fire>
2012-10-05 10:06:30 -0700

    none

--- Chapter04Mocking/tester.py
+++ Chapter04Mocking/tester.py
@@ -1,6 +1,6 @@
 import sys, doctest
 doctest.testfile(sys.argv[1])
-#raw_input()
+raw_input('Press any key to continue...')
 # again trying to get change inserted
 # making a trivial change by adding this comment so that I can see if I can 
 # commit and push
